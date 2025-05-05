416544b3927621da30bbb4066db90543b32fbb65
markkoe <rmdabs@gmail.com>
2025-05-05 16:22:08 +0800

    fix: lint staged test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>This is the Home Page!</h1>
       <Link href="/about">About</Link>
     </div>
   );
